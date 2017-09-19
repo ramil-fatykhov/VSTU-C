79bbb0dd71cee3fe5847c8f9793f36cc8e3b8801
redbarronrustik <redbarronrustik@users.noreply.github.com>
2017-09-19 16:43:11 +0300

    Added sfml2.1

---
+++ sfml.2/sfml2.1/main.cpp
@@ -0,0 +1,63 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <cmath>
+#include <iostream>
+
+constexpr unsigned WINDOW_WIDTH = 800;
+constexpr unsigned WINDOW_HEIGHT = 600;
+
+int main()
+{
+    constexpr float BALL_SIZE = 40;
+
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Wawe Moving Ball");
+    sf::Clock clock;
+
+    const sf::Vector2f position = {10, 350};
+
+    sf::CircleShape ball(BALL_SIZE);
+    ball.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+    float speedX = 100.f;
+
+    float time = 0;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        constexpr float amplitudeY = 80.f;
+        constexpr float periodY = 2;
+
+        const float dt = clock.restart().asSeconds();
+
+        time = time + dt;
+
+        const float wavePhase = time * float(2 * M_PI);
+        float x = x + speedX * dt;
+        float y = amplitudeY * std::sin(wavePhase / periodY);
+        sf::Vector2f offset = {x, y};
+
+        if ((x + 2 * BALL_SIZE >= 800) && (speedX > 0))
+        {
+            speedX = -speedX;
+        }
+        if ((x < 0) && (speedX < 0))
+        {
+            speedX = -speedX;
+        }
+
+        ball.setPosition(position + offset);
+
+        window.clear();
+        window.draw(ball);
+        window.display();
+    }
+}
