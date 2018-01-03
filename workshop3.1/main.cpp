d7462bbf42c8f91d3832be45cdc877e5c2ed5899
Ramil Fatykhov <ramikr@live.ru>
2018-01-03 11:37:41 +0300

    workshop3.1.2 added

--- workshop3.1/main.cpp
+++ workshop3.1/main.cpp
@@ -24,6 +24,24 @@ void drawFrame(sf::RenderWindow &window, sf::CircleShape &circle, sf::Text &text
     window.display();
 }
 
+void update(sf::Clock &clock, sf::Vector2f &position, sf::Vector2f &speed, sf::CircleShape &circle, sf::Text &text)
+{
+    const float dt = clock.restart().asSeconds();
+    speed.y += dt * 9.8f;
+
+    position += dt * speed;
+    if ((position.y + 2 * 80 >= 380) && (speed.y > 0))
+    {
+        speed.y = -speed.y;
+    }
+    if ((position.y < 0) && (speed.y < 0))
+    {
+        speed.y = -speed.y;
+    }
+    circle.setPosition(position);
+    text.setPosition(position);
+}
+
 int main()
 {
 
@@ -42,9 +60,14 @@ int main()
     sf::CircleShape circle(80);
     initCircle(circle);
 
+    sf::Clock clock;
+    sf::Vector2f position = {480, 380};
+    sf::Vector2f speed = {0, -80};
+
     while (window.isOpen())
     {
         pollEvents(window);
+        update(clock, position, speed, circle, text);
         drawFrame(window, circle, text);
     }
 }
